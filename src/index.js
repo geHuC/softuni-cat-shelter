433e403d41f610f8b3dc93a5f369bec04f411aca
geHuC <79841526+geHuC@users.noreply.github.com>
2021-10-02 12:49:51 +0100

    Added a public folder for static files (css, img etc)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const initHandlebars = require('./config/hadlebars.js');
-
-
-
+const path = require('path');
 
 //Setup
 const app = express();
 initHandlebars(app);
+//require('./config/hadlebars.js')(app); alt way of doing it in one row 
+
+app.use(express.static(path.resolve(__dirname,'./public')));
+console.log(path.resolve(__dirname));
 
 app.all('/', (req, res) => {
     res.render('index');

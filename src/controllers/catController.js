b57c9d3ae3a0620c86d22729ed3db08a7f40e0a4
geHuC <79841526+geHuC@users.noreply.github.com>
2021-10-02 15:07:50 +0100

    added addbreed view to catController

--- src/controllers/catController.js
+++ src/controllers/catController.js
@@ -5,7 +5,13 @@ const addCat = (req, res) => {
     res.render('addCat');
 }
 
+const addBreed = (req, res) =>
+{
+    res.render('addBreed');
+}
+
 router.get('/add-cat', addCat);
+router.get('/add-breed', addBreed);
 
 module.exports = router;
 

01f7483696b2eecc903533aab6d26a128e4cd160
geHuC <79841526+geHuC@users.noreply.github.com>
2021-10-03 19:54:08 +0100

    Added an option to delete by id in the cat service

--- src/services/catService.js
+++ src/services/catService.js
@@ -16,10 +16,15 @@ const all = async () => {
 const getById = async (id) => {
     return await Cat.findById(id).populate('breed').lean();
 }
+const deleteById = async (id) => {
+    return await Cat.findByIdAndDelete(id);
+    //should delete picture here
+}
 const catSevice = {
     create,
     all,
     getById,
+    deleteById
 }
 
 module.exports = catSevice;

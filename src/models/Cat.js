d7e4302329d886e713f447964bad7a922d3adce1
geHuC <79841526+geHuC@users.noreply.github.com>
2021-10-03 14:19:54 +0100

    Added model for Cats

---
+++ src/models/Cat.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const catSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    image: String,
+    breed: {type: mongoose.Schema.Types.ObjectId, ref:'Breed'}
+});
+
+const Cat = mongoose.model('Cat', catSchema);
+
+module.exports = Cat;

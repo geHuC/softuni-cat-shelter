8eedcbd0b2b33a31ec2aa49d4198115f869a1540
geHuC <79841526+geHuC@users.noreply.github.com>
2021-10-03 14:01:13 +0100

    Connected addbreed page to the database
    Added the addbreed page functionality to create breed and add it to the database.

--- src/controllers/catController.js
+++ src/controllers/catController.js
@@ -1,17 +1,33 @@
 const express = require('express');
+const Breed = require('../Models/Breed.js');
 const router = express.Router();
 
-const addCat = (req, res) => {
+const addCatView = (req, res) => {
     res.render('addCat');
 }
 
-const addBreed = (req, res) =>
-{
+const addBreedView = (req, res) => {
     res.render('addBreed');
 }
 
-router.get('/add-cat', addCat);
-router.get('/add-breed', addBreed);
+const addBreedPost = async (req, res) => {
+    let { breed } = req.body;
+
+    if(await Breed.exists({breed: breed})){
+        console.log(breed, ' already exists in the database');
+        res.redirect('/');
+        return;
+    }
+
+    let newBreed = new Breed({breed: breed});
+    await newBreed.save();
+    res.redirect('/');
+}
+
+router.get('/add-cat', addCatView);
+router.get('/add-breed', addBreedView);
+router.post('/add-breed', addBreedPost);
+
 
 module.exports = router;
 

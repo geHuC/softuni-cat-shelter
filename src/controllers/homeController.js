d2811b826547513e0a60a029c156428a1ba49410
geHuC <79841526+geHuC@users.noreply.github.com>
2021-10-03 19:08:21 +0100

    Changed home page with partials
    Populated the homepage using data from the db with the help of partials

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const catSevice = require('../services/catService.js');
 const router = express.Router();
 
-const home = (req, res) => {
-    res.render('index');
+const home = async (req, res) => {
+    let cats = await catSevice.all();
+    res.render('index',{cats});
 }
 
 router.get('/', home);
